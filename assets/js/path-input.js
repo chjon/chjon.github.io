f1c8f049a97fc553f0fe82a0425a7d9818980209
Jonathan Chung <jonathanchung0115@gmail.com>
2019-02-23 15:50:43 -0500

    Added transformations for path input

--- assets/js/path-input.js
+++ assets/js/path-input.js
@@ -1,3 +1,4 @@
+import * as maths from './math-utils.js';
 import * as sketch from './sketch.js';
 import { Quadtree } from './quadtree.js';
 
@@ -8,6 +9,9 @@ const modeLabel = document.getElementById('mode-label');
 let mode = 'Drawing';
 let selected = undefined;
 let closest = undefined;
+let prevPoint = undefined;
+let selection = [];
+let mousePos = { x: 0, y: 0 };
 
 // Connect the two given objects
 function connect(a, b) {
@@ -53,21 +57,109 @@ function printFrom(startPoint) {
   console.log(JSON.stringify(toPrint));
 }
 
+function move(xShift, yShift) {
+  selection.forEach((obj) => {
+    clickedTree.pop(obj);
+    obj.x += xShift;
+    obj.y += yShift;
+    clickedTree.push(obj);
+  });
+}
+
+function scale(x1, y1, x2, y2) {
+  const selectionComponents = selection.reduce((selectionComponents, obj) => {
+    selectionComponents.x.push(obj.x);
+    selectionComponents.y.push(obj.y);
+    return selectionComponents;
+  }, { x: [], y: [] });
+
+  function getCenter(list) {
+    const bounds = maths.getBounds(list);
+    return maths.avg(bounds.min, bounds.max);
+  }
+
+  const mid = {
+    x: getCenter(selectionComponents.x),
+    y: getCenter(selectionComponents.y),
+  }
+
+  const xScale = (x2 - mid.x) / (x1 - mid.x);
+  const yScale = (y2 - mid.y) / (y1 - mid.y);
+
+  selection.forEach((obj) => {
+    clickedTree.pop(obj);
+    obj.x = (obj.x - mid.x) * xScale + mid.x;
+    obj.y = (obj.y - mid.y) * yScale + mid.y;
+    clickedTree.push(obj);
+  });
+}
+
+function rotate(x1, y1, x2, y2) {
+  const selectionComponents = selection.reduce((selectionComponents, obj) => {
+    selectionComponents.x.push(obj.x);
+    selectionComponents.y.push(obj.y);
+    return selectionComponents;
+  }, { x: [], y: [] });
+
+  function getCenter(list) {
+    const bounds = maths.getBounds(list);
+    return maths.avg(bounds.min, bounds.max);
+  }
+
+  const mid = {
+    x: getCenter(selectionComponents.x),
+    y: getCenter(selectionComponents.y),
+  }
+
+  const shiftAngle = Math.atan2(y2 - mid.y, x2 - mid.x) - Math.atan2(y1 - mid.y, x1 - mid.x);
+  const cosAngle = Math.cos(shiftAngle);
+  const sinAngle = Math.sin(shiftAngle);
+
+  selection.forEach((obj) => {
+    clickedTree.pop(obj);
+    obj.x -= mid.x;
+    obj.y -= mid.y;
+    const newX = obj.x * cosAngle - obj.y * sinAngle + mid.x;
+    const newY = obj.x * sinAngle + obj.y * cosAngle + mid.y;
+    obj.x = newX;
+    obj.y = newY;
+    clickedTree.push(obj);
+  });
+}
+
+function deselect(obj) {
+  const objIndex = selection.findIndex((testObj) => {
+    return testObj === obj;
+  });
+  if (obj && objIndex !== -1) {
+    selection.splice(objIndex, 1);
+  }
+}
+
+function select(obj) {
+  const objIndex = selection.findIndex((testObj) => {
+    return testObj === obj;
+  });
+  if (obj && objIndex === -1) {
+    selection.push(obj);
+  }
+}
+
 // Draw the objects in the quadtree
-function drawQuadtree(qTree) {
+function drawQuadtree(qTree, pointSize) {
   if (qTree.bucket) {
     qTree.bucket.forEach((storedObj) => {
-      sketch.ellipse(storedObj.x, storedObj.y, 5, 5);
+      sketch.ellipse(storedObj.x, storedObj.y, pointSize, pointSize);
 
       if (storedObj.next) {
         sketch.line(storedObj.x, storedObj.y, storedObj.next.x, storedObj.next.y);
       }
     });
   } else {
-    drawQuadtree(qTree.ne);
-    drawQuadtree(qTree.nw);
-    drawQuadtree(qTree.se);
-    drawQuadtree(qTree.sw);
+    drawQuadtree(qTree.ne, pointSize);
+    drawQuadtree(qTree.nw, pointSize);
+    drawQuadtree(qTree.se, pointSize);
+    drawQuadtree(qTree.sw, pointSize);
   }
 }
 
@@ -77,6 +169,8 @@ document.onmousemove = (e) => {
   }
 
   closest = clickedTree.getClosestWithin(e.pageX, e.pageY, GRAB_RADIUS * GRAB_RADIUS);
+  mousePos.x = e.pageX;
+  mousePos.y = e.pageY;
 }
 
 document.onmousedown = (e) => {
@@ -106,8 +200,49 @@ document.onmousedown = (e) => {
       connect(selected, closest);
       selected = closest;
     }
+  } else if (mode === 'Moving') {
+    if (prevPoint) {
+      move(e.pageX - prevPoint.x, e.pageY - prevPoint.y);
+      prevPoint = undefined;
+    } else {
+      prevPoint = { x: e.pageX, y: e.pageY };
+    }
   } else if (mode === 'Printing') {
     printFrom(closest);
+  } else if (mode === 'Rotating') {
+    if (prevPoint) {
+      rotate(prevPoint.x, prevPoint.y, e.pageX, e.pageY);
+      prevPoint = undefined;
+    } else {
+      prevPoint = { x: e.pageX, y: e.pageY };
+    }
+  } else if (mode === 'Selecting') {
+    if (closest) {
+      if (e.ctrlKey) {
+          deselect(closest);
+        } else {
+          select(closest);
+        }
+    } else if (prevPoint) {
+      const foundPoints = clickedTree.getAllWithin(prevPoint.x, prevPoint.y, e.pageX, e.pageY);
+      foundPoints.forEach((obj) => {
+        if (e.ctrlKey) {
+          deselect(obj);
+        } else {
+          select(obj);
+        }
+      });
+      prevPoint = undefined;
+    } else {
+      prevPoint = { x: e.pageX, y: e.pageY };
+    }
+  } else if (mode === 'Scaling') {
+    if (prevPoint) {
+      scale(prevPoint.x, prevPoint.y, e.pageX, e.pageY);
+      prevPoint = undefined;
+    } else {
+      prevPoint = { x: e.pageX, y: e.pageY };
+    }
   }
 }
 
@@ -117,17 +252,33 @@ document.onkeyup = (e) => {
   }
 
   switch (e.keyCode) {
+    // Delete selection
+    case 46:
+      selection.forEach((obj) => {
+        clickedTree.pop(obj);
+      });
+      selection = [];
+      break;
+
     // Switch to drawing mode
     case 'D'.charCodeAt(0):
       mode = 'Drawing';
       selected = undefined;
+      selection = [];
       break;
 
     // Switch to connecting mode
     case 'C'.charCodeAt(0):
       mode = 'Connecting';
+      selection = [];
       break;
     
+    // Switch to moving mode
+    case 'M'.charCodeAt(0):
+      mode = 'Moving';
+      selected = undefined;
+      break;
+
     // Output the raw point data
     case 'O'.charCodeAt(0):
       console.log(clickedTree.getAll());
@@ -136,6 +287,30 @@ document.onkeyup = (e) => {
     // Output the points in order of connection if all of them are connected
     case 'P'.charCodeAt(0):
       mode = 'Printing';
+      break;
+
+    // Switch to rotating mode
+    case 'R'.charCodeAt(0):
+      mode = 'Rotating';
+      selected = undefined;
+      break;
+
+    // Switch to selecting mode
+    case 'S'.charCodeAt(0):
+      mode = 'Selecting';
+      selected = undefined;
+      break;
+
+    // Switch to scaling mode
+    case 'T'.charCodeAt(0):
+      mode = 'Scaling';
+      selected = undefined;
+      break;
+
+    case 'V'.charCodeAt(0):
+      mode = 'Viewing';
+      selected = undefined;
+      selection = [];
       break;
 
     default:
@@ -152,7 +327,11 @@ function setup() {
 
 function draw() {
   sketch.setStroke('#FFFFFF');
-  drawQuadtree(clickedTree);
+  if (mode === 'Viewing') {
+    drawQuadtree(clickedTree, 1);
+  } else {
+    drawQuadtree(clickedTree, 5);
+  }
 
   // Draw the closest point
   if (closest) {
@@ -171,6 +350,20 @@ function draw() {
     if (selected.prev) {
       sketch.setStroke('#FF0000');
       sketch.ellipse(selected.prev.x, selected.prev.y, GRAB_RADIUS, GRAB_RADIUS);
+    }
+  }
+
+  // Draw all selected points
+  sketch.setStroke('#FFFFFF');
+  selection.forEach((obj) => {
+    sketch.ellipse(obj.x, obj.y, GRAB_RADIUS, GRAB_RADIUS);
+  });
+
+  if (prevPoint) {
+    if (mode === 'Selecting') {
+      sketch.rect(prevPoint.x, prevPoint.y, mousePos.x, mousePos.y);
+    } else if (mode === 'Moving' || mode === 'Scaling' || mode === 'Rotating') {
+      sketch.line(prevPoint.x, prevPoint.y, mousePos.x, mousePos.y);
     }
   }
 }
