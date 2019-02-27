d0510de839e38a1d98a11fb63e20a5cfc4ab2507
Jonathan Chung <jonathanchung0115@gmail.com>
2019-02-27 00:32:05 -0500

    started I/O for resistors

--- assets/js/resistors.js
+++ assets/js/resistors.js
@@ -1,12 +1,57 @@
-import * as sketch from './sketch.js';
+let desiredResistanceInput;
+let maxResistorsInput;
+let availableResistanceInput;
+let calculateButtonInput;
+let combinationOutput;
 
-function setup() {
+const TOLERANCE = 0.05;
 
+function validate(desiredResistance, availableResistances) {
+  // Validate inputs
+  if (!desiredResistance) {
+    alert('Enter a valid desired resistance');
+    return false;
+  } else if (!availableResistances) {
+    alert('Enter available resistances');
+    return false;
+  }
+
+  return true;
 }
 
-function draw() {
-  sketch.setStroke('#FFFFFF');
-  sketch.rect(0, 0, 300, 300);
+function getResistanceList(availableResistances) {
+  return availableResistances
+  .replace(/\s|\n|\r/gi, '')
+  .split(',')
+  .filter((s) => {
+    return s.length;
+  })
+  .map((s) => {
+    return parseFloat(s);
+  });
 }
 
-sketch.init(setup, draw, 'resistor-color-decoder', 300, 300);
+function recalculate() {
+  const desiredResistance = desiredResistanceInput.value;
+  const maxResistors = maxResistorsInput.value;
+  const availableResistances = availableResistanceInput.value;
+
+  if (!validate(desiredResistance, availableResistances)) {
+    return;
+  }
+
+  const resistancesList = getResistanceList(availableResistances);
+
+  console.log(resistancesList);
+  combinationOutput.value = resistancesList;
+}
+
+window.onload = () => {
+  desiredResistanceInput = document.getElementById('desired-resistance');
+  maxResistorsInput = document.getElementById('max-resistors');
+  availableResistanceInput = document.getElementById('available-resistances');
+  calculateButtonInput = document.getElementById('calculate-button');
+  combinationOutput = document.getElementById('combo-output');
+
+  calculateButtonInput.onclick = recalculate;
+}
--- projects/resistors.md
+++ projects/resistors.md
@@ -3,12 +3,26 @@ layout: default
 stylesheet: /assets/css/default.css
 script: /assets/js/resistors.js
 ---
-{% capture resistor-color-decoder %}
-  <canvas id="resistor-color-decoder"></canvas>
-  <p>Test</p>
+{% capture input-panel %}
+  <p>Desired resistance</p>
+  <input id="desired-resistance" type="number" min="1" value="1">
+  <p>Maximum number of resistors:</p>
+  <input id="max-resistors" type="range" min="1" max="5" value="3">
+  <p>Available resistances (comma separated)</p>
+  <textarea id="available-resistances" rows="8"></textarea>
+  <input id="calculate-button" type="button" value="Calculate">
 {% endcapture %}
-<div>
+
+{% capture output-panel %}
+  <p>Possible combinations</p>
+  <textarea id="combo-output" rows="8" readonly="true"></textarea>
+{% endcapture %}
+
+<div class="row">
   {% include card.html
-    content=resistor-color-decoder
+    content=input-panel
+  %}
+  {% include card.html
+    content=output-panel
   %}
 </div>
