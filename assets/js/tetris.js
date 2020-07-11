aefefa0c19e2d5c8a609909362bbf409dc1d950f
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-07-11 18:24:41 -0400

    use css for styling

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -1,103 +1,102 @@
-import * as sketch from './sketch.js';
-
 const FPS = 60;
-let titleString = "Tetris"
+let canvases;
 let tetris;
 
+function line(ctx, x1, y1, x2, y2) {
+	ctx.save();
+	ctx.beginPath();
+
+	ctx.moveTo(x1, y1);
+	ctx.lineTo(x2, y2);
+
+	ctx.closePath();
+	ctx.restore();
+	ctx.stroke();
+}
+
+class PreviewCanvas {
+	constructor(previewCanvas) {
+		this.ctx = previewCanvas;
+	}
+
+	onResize() {
+
+	}
+
+	draw() {
+
+	}
+}
+
 class Tetris {
-	constructor(minPaddingRatio) {
-		this.minPaddingRatio = minPaddingRatio;
-		this.uiRatio = { left: 0.2, centre: 0.4, right: 0.2 };
+	constructor(canvases) {
+		this.holdCanvas = new PreviewCanvas(canvases.hold);
+		this.nextCanvas = new PreviewCanvas(canvases.next);
+		this.ctx = canvases.game;
 		this.gridDim = { x: 10, y: 20 };
 	}
 
-	onResize(width, height) {
-		this.dim = { x: width, y: height };
-		const minPadding = Math.min(this.minPaddingRatio.x * width, this.minPaddingRatio.y * height);
-		const padding = { x: minPadding, y: minPadding };
-		this.titleSize = Math.floor(padding.y / 2);
-		this.titlePos = { x: width / 2, y: padding.y / 2 };
-		this.textSize = Math.floor(padding.y / 4);
-		const renderBounds = {
-			pos: padding,
-			dim: { x: width - 2 * padding.x, y: height - 2 * padding.y }
-		};
-
+	onResize() {
 		// Calculate grid size and position
-		const gridRenderBounds = {
-			pos: { x: ( 1 - this.uiRatio.centre) * renderBounds.dim.x / 2  + renderBounds.pos.x, y: renderBounds.pos.y },
-			dim: { x:       this.uiRatio.centre  * renderBounds.dim.x,                           y: renderBounds.dim.y }
-		};
-		const tileDim = Math.min(gridRenderBounds.dim.x / this.gridDim.x, gridRenderBounds.dim.y / this.gridDim.y);
-		this.gridTileDim  = { x: tileDim,                  y: tileDim };
-		this.gridPixelDim = { x: tileDim * this.gridDim.x, y: tileDim * this.gridDim.y };
-		this.gridPixelPos = {
-			x: gridRenderBounds.pos.x + gridRenderBounds.dim.x / 2 - this.gridPixelDim.x / 2,
-			y: gridRenderBounds.pos.y + gridRenderBounds.dim.y / 2 - this.gridPixelDim.y / 2
-		};
-		
-		// Calculate the upper left hand corner of the tetris game
-		this.pixelDim = renderBounds.dim;
-		this.pos = renderBounds.pos;		
+		this.gridTileDim  = { x: this.ctx.canvas.width / this.gridDim.x, y: this.ctx.canvas.height / this.gridDim.y };
+		this.gridPixelDim = { x: this.ctx.canvas.width, y: this.ctx.canvas.height };	
 	}
 
 	init() {
-		this.onResize(sketch.getWidth(), sketch.getHeight());
+		this.onResize();
 	}
 
 	drawGameGrid() {
-		sketch.setStroke("#FFFFFF");
+		this.ctx.strokeStyle = "#FFFFFF";
 		// Draw vertical lines
 		for (let x = 0; x <= this.gridDim.x; ++x) {
-			sketch.line(
-				Math.round(this.gridPixelPos.x + x * this.gridTileDim.x), Math.round(this.gridPixelPos.y),
-				Math.round(this.gridPixelPos.x + x * this.gridTileDim.x), Math.round(this.gridPixelPos.y + this.gridPixelDim.y)
+			line(this.ctx,
+				Math.round(x * this.gridTileDim.x), Math.round(0),
+				Math.round(x * this.gridTileDim.x), Math.round(this.gridPixelDim.y)
 			);
 		}
 		// Draw horizontal lines
 		for (let y = 0; y <= this.gridDim.y; ++y) {
-			sketch.line(
-				Math.round(this.gridPixelPos.x),                       Math.round(this.gridPixelPos.y + y * this.gridTileDim.y),
-				Math.round(this.gridPixelPos.x + this.gridPixelDim.x), Math.round(this.gridPixelPos.y + y * this.gridTileDim.y)
+			line(this.ctx,
+				Math.round(0),                   Math.round(y * this.gridTileDim.y),
+				Math.round(this.gridPixelDim.x), Math.round(y * this.gridTileDim.y)
 			);
 		}
 	}
 
 	draw() {
-		// Draw title
-		sketch.setFill("#FFFFFF");
-		sketch.textStyle(`${this.titleSize}px Arial`);
-		sketch.textAlign("center");
-		sketch.textBaseline("middle");
-		sketch.text(this.titlePos.x, this.titlePos.y, titleString, true);
-
-		// Draw render bounds
-		sketch.setFill("#333333");
-		sketch.fillRect(
-			this.pos.x,                   this.pos.y,
-			this.pos.x + this.pixelDim.x, this.pos.y + this.pixelDim.y,
-		);
-
 		// Draw game grid
 		this.drawGameGrid();
 	}
 }
 
-function draw() {
-	// Draw game
-	tetris.draw();
+function setUpCanvas(canvasName) {
+	let canvas = document.getElementById(canvasName);
+	canvas.width = canvas.offsetWidth;
+	canvas.height = canvas.offsetHeight;
+	return canvas.getContext('2d');
 }
 
-function onResize(width, height) {
-	tetris.onResize(width, height);
-}
-
-function setup() {
-	sketch.setFrameInterval(1000 / FPS);
-	tetris = new Tetris({ x: 0.1, y: 0.1 });
-	onResize(sketch.getWidth(), sketch.getHeight());
+window.onload = () => {
+	canvases = {
+		hold: setUpCanvas("hold-canvas"),
+		next: setUpCanvas("next-canvas"),
+		game: setUpCanvas("game-canvas")
+	};
+	tetris = new Tetris(canvases);
 	tetris.init();
+	setInterval(() => {
+		Object.values(canvases).forEach((ctx) => {
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+			tetris.draw();
+		});
+	}, 1000 / FPS);
 }
 
-sketch.onResize(onResize);
-sketch.init(setup, draw);
+window.onresize = () => {
+	Object.values(canvases).forEach((ctx) => {
+		ctx.canvas.width  = ctx.canvas.offsetWidth;
+		ctx.canvas.height = ctx.canvas.offsetHeight;
+	});
+	tetris.onResize();
+};
--- projects/tetris.md
+++ projects/tetris.md
@@ -1,8 +1,44 @@
 ---
 override_title: true
 title: tetris
-layout: canvas-page
-stylesheet: /assets/css/default.css
+layout: empty
+override_stylesheet: true
+stylesheet: /assets/css/tetris.css
 script: /assets/js/tetris.js
 ---
-<div></div>
+<div class="title-card">
+	<h1 class="title" id="title">Tetris</h1>
+</div>
+<div class="game-container">
+	<div class="ui-panel">
+		<h2>Hold</h2>
+		<div class="padding">
+			<div class="square-wrapper">
+				<div class="square-padding"></div>
+				<canvas class="square-content piece-display" id="hold-canvas"></canvas>
+			</div>
+		</div>
+		<div class="score-display">
+			<h3>Lines:</h3>
+			<p>0</p>
+		</div>
+		<div class="score-display">
+			<h3>Score:</h3>
+			<p>0</p>
+		</div>
+	</div>
+	<div class="tetris-grid">
+		<div class="game-padding">
+			<canvas class="game-canvas" id="game-canvas"></canvas>
+		</div>
+	</div>
+	<div class="ui-panel">
+		<h2>Next</h2>
+		<div class="padding">
+			<div class="square-wrapper">
+				<div class="square-padding"></div>
+				<canvas class="square-content piece-display" id="next-canvas"></canvas>
+			</div>
+		</div>
+	</div>
+</div>
