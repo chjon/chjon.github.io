8c6b36175956e97636a9a5acfdf11e812ae7739a
Jonathan Chung <jt2chung@uwaterloo.ca>
2020-07-11 23:28:30 -0400

    render piece previews

--- assets/js/tetris.js
+++ assets/js/tetris.js
@@ -1,6 +1,15 @@
 const FPS = 60;
 let canvases;
 let tetris;
+const gamePieces = {
+	i: [[0, 0, 0, 0],[1, 1, 1, 1],[0, 0, 0, 0],[0, 0, 0, 0]],
+	j: [[0, 0, 0, 0],[1, 1, 1, 0],[0, 0, 1, 0],[0, 0, 0, 0]],
+	l: [[0, 0, 0, 0],[0, 1, 1, 1],[0, 1, 0, 0],[0, 0, 0, 0]],
+	o: [[0, 0, 0, 0],[0, 1, 1, 0],[0, 1, 1, 0],[0, 0, 0, 0]],
+	s: [[0, 0, 0, 0],[0, 0, 1, 1],[0, 1, 1, 0],[0, 0, 0, 0]],
+	t: [[0, 0, 0, 0],[0, 0, 1, 0],[0, 1, 1, 0],[0, 0, 1, 0]],
+	z: [[0, 0, 0, 0],[1, 1, 0, 0],[0, 1, 1, 0],[0, 0, 0, 0]],
+};
 
 function line(ctx, x1, y1, x2, y2) {
 	ctx.save();
@@ -14,17 +23,102 @@ function line(ctx, x1, y1, x2, y2) {
 	ctx.stroke();
 }
 
+function arrayCopy2D(collisionMap) {
+	let copy = [];
+	for (let i = 0; i < collisionMap.length; ++i) {
+		copy.push([]);
+		for (let j = 0; j < collisionMap[i].length; ++j) {
+			copy[i].push(collisionMap[i][j]);
+		}
+	}
+	return copy;
+}
+
+class GamePiece {
+	constructor(collisionMap, color) {
+		this.collisionMap = arrayCopy2D(collisionMap);
+		this.color = color;
+		this.dims = { x: collisionMap[0].length, y: collisionMap.length };
+
+		// Calculate width and height
+		this.widthMin = this.collisionMap[0].length - 1;
+		this.widthMax = 0;
+		this.heightMin = this.collisionMap.length - 1;
+		this.heightMax = 0;
+		for (let i = 0; i < this.collisionMap.length; ++i) {
+			for (let j = 0; j < this.collisionMap[i].length; ++j) {
+				if (this.collisionMap[i][j]) {
+					this.widthMin = Math.min(this.widthMin, j);
+					this.widthMax = Math.max(this.widthMax, j);
+					this.heightMin = Math.min(this.heightMin, i);
+					this.heightMax = Math.max(this.heightMax, i);
+				}
+			}
+		}
+		this.width  = this.widthMax  - this.widthMin  + 1;
+		this.height = this.heightMax - this.heightMin + 1;
+	}
+
+	onRotate() {
+		const tmp   = this.width;
+		this.width  = this.height;
+		this.height = tmp;
+	}
+
+	getCollisionMap() {
+		return this.collisionMap;
+	}
+}
+
 class PreviewCanvas {
 	constructor(previewCanvas) {
 		this.ctx = previewCanvas;
+		this.paddingFrac = { x: 0.05, y: 0.05 };
 	}
 
 	onResize() {
-
+		this.ctx.canvas.width  = this.ctx.canvas.offsetWidth;
+		this.ctx.canvas.height = this.ctx.canvas.offsetHeight;
+		this.dims = { x: this.ctx.canvas.width * (1 - 2 * this.paddingFrac.x), y: this.ctx.canvas.height * (1 - 2 * this.paddingFrac.y) };
+		this.pos  = { x: this.ctx.canvas.width *          this.paddingFrac.x,  y: this.ctx.canvas.height *          this.paddingFrac.y };
 	}
 
-	draw() {
+	draw(gamePiece) {
+		const tileSize = Math.min(this.dims.x / gamePiece.width, this.dims.y / gamePiece.height);
+		const offset = {
+			x: this.ctx.canvas.width  / 2 - gamePiece.width  * tileSize / 2,
+			y: this.ctx.canvas.height / 2 - gamePiece.height * tileSize / 2,
+		}
 
+		// Draw piece
+		this.ctx.fillStyle = gamePiece.color;
+		for (let c_i = gamePiece.heightMin, i = 0; c_i <= gamePiece.heightMax; ++c_i) {
+			for (let c_j = gamePiece.widthMin, j = 0; c_j <= gamePiece.widthMax; ++c_j) {
+				if (gamePiece.collisionMap[c_i][c_j]) {
+					this.ctx.fillRect(
+						Math.round(offset.x + j * tileSize), Math.round(offset.y + i * tileSize),
+						Math.round(tileSize), Math.round(tileSize)
+					);
+				}
+				++j;
+			}
+			++i;
+		}
+
+		// Draw overlay grid
+		this.ctx.strokeStyle = "#000000";
+		for (let x = 0; x <= gamePiece.width; ++x) {
+			line(this.ctx,
+				Math.round(offset.x + x * tileSize), Math.round(0),
+				Math.round(offset.x + x * tileSize), Math.round(this.ctx.canvas.height),
+			);
+		}
+		for (let y = 0; y <= gamePiece.height; ++y) {
+			line(this.ctx,
+				Math.round(0),                     Math.round(offset.y + y * tileSize),
+				Math.round(this.ctx.canvas.width), Math.round(offset.y + y * tileSize),
+			);
+		}
 	}
 }
 
@@ -40,6 +134,9 @@ class Tetris {
 		// Calculate grid size and position
 		this.gridTileDim  = { x: this.ctx.canvas.width / this.gridDim.x, y: this.ctx.canvas.height / this.gridDim.y };
 		this.gridPixelDim = { x: this.ctx.canvas.width, y: this.ctx.canvas.height };	
+
+		this.holdCanvas.onResize();
+		this.nextCanvas.onResize();
 	}
 
 	init() {
@@ -67,6 +164,10 @@ class Tetris {
 	draw() {
 		// Draw game grid
 		this.drawGameGrid();
+
+		// Draw previews
+		this.holdCanvas.draw(new GamePiece(gamePieces.z, "#FFFFFF"));
+		this.nextCanvas.draw(new GamePiece(gamePieces.j, "#FFFFFF"));
 	}
 }
 
@@ -88,8 +189,8 @@ window.onload = () => {
 	setInterval(() => {
 		Object.values(canvases).forEach((ctx) => {
 			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-			tetris.draw();
 		});
+		tetris.draw();
 	}, 1000 / FPS);
 }
 
