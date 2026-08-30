81d3a4bf7f3932272530bb01585f751010942dad
Jonathan Chung <jonathanchung0115@gmail.com>
2026-08-30 15:36:42 -0400

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+react-router build &&
+    mv ./build/client ./build_docs &&
+    rm -rf ./build &&
+    cp -rt ./build_docs ./assets ./deps/*;
+
+git checkout gh-pages &&
+    rm -rf ./docs &&
+    mv ./build_docs ./docs;
